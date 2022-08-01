ac6c5da6a4724992a35efaa79e208d1b83846387
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-01 13:21:03 +0900

    fix Login

--- src/ui/pages/Login.tsx
+++ src/ui/pages/Login.tsx
@@ -1,46 +1,37 @@
-import { VStack } from "@chakra-ui/react";
+import { FormControl, FormLabel } from "@chakra-ui/react";
 import React from "react";
 import Box from "ui/components/Box/Box";
 import Button from "ui/components/Button/Button";
 import Center from "ui/components/Center/Center";
 import Container from "ui/components/Container/Container";
 import Divider from "ui/components/Divider/Divider";
+import { FormHelperText } from "ui/components/Form/FormHelperText";
 import Input from "ui/components/Input/Input";
+import VStack from "ui/components/Stack/VStack";
 import Text from "ui/components/Text/Text";
 import { theme } from "utils/theme";
 
 const Login: React.FC = () => {
+	const list = [
+		{ text: "Email", type: "email", placeholder: "mail@example.com" },
+		{ text: "Password", type: "password", placeholder: "abc123" },
+	];
 	return (
 		<Center h={theme.h.full}>
 			<VStack mb={100} w={theme.w.mobile}>
 				<Text fontSize={theme.fs.h3}>Login</Text>
 				<Container borderRadius={theme.borderRadius.md} border={theme.border}>
-					<Box mt={theme.m.sm} mb={theme.m.sm}>
-						<Input
-							placeholder="mail@example.com"
-							text={"Email"}
-							value="value"
-							onChange={() => {
-								console.log("onChange");
-							}}
-						/>
-					</Box>
-					<Input
-						placeholder="abc123"
-						text={"Password"}
-						value="value"
-						onChange={() => {
-							console.log("onChange");
-						}}
-					/>
-					<Text
-						textAlign="center"
-						color={theme.color.red}
-						mt={theme.m.sm}
-						mb={theme.m.sm}
-					>
-						error
-					</Text>
+					<FormControl>
+						{list.map((ele) => (
+							<Box mt={theme.m.sm} mb={theme.m.sm}>
+								<FormLabel>{ele.text}</FormLabel>
+								<Input {...ele} />
+							</Box>
+						))}
+						<Box mt={theme.m.sm} mb={theme.m.sm}>
+							<FormHelperText>Wrong something.</FormHelperText>
+						</Box>
+					</FormControl>
 					<Button
 						isFullWidth
 						mb={theme.m.sm}
