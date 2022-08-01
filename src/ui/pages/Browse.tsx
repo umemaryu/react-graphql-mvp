562ab244aef540bd3f3248c2fd9d2103aeeb77b4
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-01 18:03:00 +0900

    fix Browse

--- src/ui/pages/Browse.tsx
+++ src/ui/pages/Browse.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import WallContainer from "ui/components/Container/WallContainer";
+import { VStack } from "@chakra-ui/react";
+import Box from "ui/components/Box/Box";
+import UserInfo from "ui/components/List/UserInfo";
+import Text from "ui/components/Text/Text";
+import { theme } from "utils/theme";
+import Post from "ui/components/Post/Post";
+import Search from "ui/components/Search/Search";
 
 const Browse: React.FC = () => {
-	return <>Browse</>;
+	return (
+		<WallContainer page="Browse">
+			<Box pt={theme.m.md}>
+				<VStack spacing={theme.m.md}>
+					<UserInfo />
+					<Text textAlign="center">Search the other user by email</Text>
+					<Search />
+					<Post />
+				</VStack>
+			</Box>
+		</WallContainer>
+	);
 };
 
 export default Browse;
