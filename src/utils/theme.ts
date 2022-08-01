597e14b796c28cf28ace09729b56f400c4b31125
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-01 18:24:57 +0900

    create theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+	w: { mobile: "376px", wall: "560px" },
+	h: { full: "100vh" },
+	m: { sm: 4, md: 8, lg: 12 },
+	border: "1px solid black",
+	borderRadius: { sm: 8, md: 16, lg: 24 },
+	fs: {
+		h1: { sm: "48px", md: "52px", lg: "56px" },
+		h2: { sm: "40px", md: "44px", lg: "48px" },
+		h3: { sm: "32px", md: "36px", lg: "40px" },
+		h4: { sm: "24px", md: "28px", lg: "32px" },
+		h5: { sm: "20px", md: "24px", lg: "28px" },
+		h6: { sm: "16px", md: "20px", lg: "24px" },
+		normal: "16px",
+		xs: "12px",
+	},
+	color: {
+		blue: "#3182CE",
+	},
+};
