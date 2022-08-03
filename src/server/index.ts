cc17171a510522275b023eefd4e3bffd94446aae
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-03 10:38:46 +0900

    define createUser

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,38 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { ApolloServer } from "apollo-server";
 import { readFileSync } from "fs";
+import { MutationCreateUserArgs } from "server/codegen";
 
-const prisma = new PrismaClient();
 const syncedTypeDefs = readFileSync("src/gql/schema.gql", "utf8");
 const typeDefs = `${syncedTypeDefs}`;
-
+const prisma = new PrismaClient();
 const resolvers = {
 	Query: {},
+	Mutation: {
+		createUser: async (_parent: unknown, args: MutationCreateUserArgs) => {
+			try {
+				if (args.password.length < 6)
+					throw new Error("The password must be over 6 characters");
+				const token = "aaaaa";
+				const res = await prisma.user.create({
+					data: {
+						email: args.email,
+						password: args.password,
+						country: args.country,
+						city: args.city,
+						nickName: args.nickName,
+						token: token,
+					},
+				});
+				return res;
+			} catch (e) {
+				if (e instanceof Prisma.PrismaClientKnownRequestError)
+					throw new Error("The email is already used");
+				else if (e instanceof Error) throw new Error(e.message);
+				throw new Error("unknown Error");
+			}
+		},
+	},
 };
 
 const server = new ApolloServer({ resolvers, typeDefs });
