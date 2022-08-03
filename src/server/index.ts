d3d01006d7520ee5ab3cdcd2b9425af9082cfa7d
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-03 09:49:20 +0900

    fix code

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,7 @@ import { ApolloServer } from "apollo-server";
 import { readFileSync } from "fs";
 
 const prisma = new PrismaClient();
-const syncedTypeDefs = readFileSync("src/graphql/schema.gql", "utf8");
+const syncedTypeDefs = readFileSync("src/gql/schema.gql", "utf8");
 const typeDefs = `${syncedTypeDefs}`;
 
 const resolvers = {
