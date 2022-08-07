5fd9c5c74503a3264276da29b5f82e102d67251a
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-07 12:52:26 +0900

    create usePost

---
+++ src/application/usePost.ts
@@ -0,0 +1,23 @@
+import {
+	FetchUserByTokenQuery,
+	MutationCreatePostArgs,
+	useCreatePostMutation,
+} from "gql/codegen";
+
+type Input = { models: FetchUserByTokenQuery | undefined };
+
+const usePost = ({ models }: Input) => {
+	const [CREATE_POST_MUTATION] = useCreatePostMutation();
+	const createPost = async (args: MutationCreatePostArgs) => {
+		try {
+			return await CREATE_POST_MUTATION({
+				variables: args,
+			}).then((res) => res.data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+	return { createPost };
+};
+
+export default usePost;
