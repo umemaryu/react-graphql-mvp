76167b1ec568569df76f513dec9a05172d2053cd
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-07 22:40:57 +0900

    fix error

--- src/containers/Account.container.tsx
+++ src/containers/Account.container.tsx
@@ -4,5 +4,5 @@ import { AccountSection } from "components/Sections";
 export const Account = () => {
 	const { operations } = useAuth();
 	const { updateTokenToNull } = operations;
-	return <AccountSe1ction actions={{ updateTokenToNull }} />;
+	return <AccountSection actions={{ updateTokenToNull }} />;
 };
