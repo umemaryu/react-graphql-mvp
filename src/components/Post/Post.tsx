40188d258b8c11b2d071cb180f3350b8198220eb
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-07 13:19:35 +0900

    add ui logic

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, HStack, PostIcon } from "components/Elements";
 import { theme } from "utils/theme";
 import { Textarea } from "components/Elements/Textarea";
+import { ProfileActions } from "containers";
 
-export const Post: React.FC = () => {
+type Props = ProfileActions | any;
+
+const usePost = ({ actions }: ProfileActions) => {
+	const [value, setValue] = useState<string>("");
+	const onChange = (value: string) => {
+		setValue(value);
+	};
+	const onClick = () => {};
+	return { models: { value }, operations: { onClick, onChange } };
+};
+
+export const Post: React.FC<Props> = ({ actions }) => {
+	const { models, operations } = usePost({ actions });
 	return (
 		<HStack
 			border="1px"
@@ -13,12 +26,14 @@ export const Post: React.FC = () => {
 			bottom="0vh"
 			bg={theme.color.white}
 		>
-			<Textarea placeholder="Hi!" my={2} ml={2} />
-			<Box
-				onClick={() => {
-					console.log("onClick");
-				}}
-			>
+			<Textarea
+				placeholder="Hi!"
+				my={2}
+				ml={2}
+				onChange={(e) => operations.onChange(e.target.value)}
+				value={models.value}
+			/>
+			<Box onClick={() => operations.onClick()}>
 				<PostIcon />
 			</Box>
 		</HStack>
