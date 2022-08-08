461c5f18ff4f21be00fa28b24df4f27ae782cb37
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-08 10:01:41 +0900

    add useAuth

--- src/containers/Browse.container.tsx
+++ src/containers/Browse.container.tsx
@@ -1,7 +1,8 @@
-import { usePost, useUser } from "application";
+import { useAuth, usePost, useUser } from "application";
 import { BrowseSection } from "components/Sections";
 
 export const Browse = () => {
+	useAuth();
 	const { models, operations: userOperations } = useUser();
 	const { operations: postOperations } = usePost({});
 	const { fetchUserByEmail } = userOperations;
