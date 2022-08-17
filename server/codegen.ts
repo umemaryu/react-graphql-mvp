3637783640863fd48be2323c9cad409ab0a8f9cc
ryutaro1069@gmail.com <ryutaro1374@icloud.com>
2022-08-17 11:59:52 +0900

    move models to server folder

--- server/codegen.ts
+++ server/codegen.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo } from 'graphql';
-import { UserPost, UserModel, Context } from 'infra/models';
+import { UserPost, UserModel, Context } from 'server/models';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { ApolloServer, UserInputError, ValidationError } from "apollo-server";
 import { readFileSync } from "fs";
-import { Context } from "infra/models";
+import { Context } from "../server/models";
 import {
 	MutationCreatePostArgs,
 	MutationCreateUserArgs,
